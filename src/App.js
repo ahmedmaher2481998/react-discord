a681eb4fbe50393be25a0e13959beb60045ac0f3
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-05-16 13:24:56 +0200

    hero page text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Header";
+import Hero from "./Components/Hero";
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<Hero />
 		</div>
 	);
 }
